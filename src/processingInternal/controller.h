ef88e9fe9e097d0498d9d66824b5053267dfeeeb
schirmermischa <schirmer@mpia.de>
2019-11-15 16:17:22 +0100

    Licensed all source files under GPL v3 and later

--- src/processingInternal/controller.h
+++ src/processingInternal/controller.h
@@ -1,3 +1,22 @@
+/*
+Copyright (C) 2019 Mischa Schirmer
+
+This file is part of THELI.
+
+THELI is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program in the LICENSE file.
+If not, see https://www.gnu.org/licenses/ .
+*/
+
 // The controller class keeps track of all data defined in the data directory tree,
 // and whether it is kept in memory or on disk, throughout the processing.
 // It also defines all the processing functions, kept separately in processing.cc
