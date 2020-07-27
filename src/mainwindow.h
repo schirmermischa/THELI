f32801b02f49b4631d09ca3964c6e76e7cf19a14
schirmermischa <schirmer@mpia.de>
2020-07-27 10:39:08 +0200

    Pushed version number to v3.0.3

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -73,7 +73,7 @@ public:
     QString instrument_dir;
     QString instrument_userDir;
 
-    QString GUIVERSION = "3.0.2";
+    QString GUIVERSION = "3.0.3";
 
     QString editorPreference;
     QString downloadServer;
